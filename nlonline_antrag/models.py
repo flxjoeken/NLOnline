723f8d39bf4a3dcd2472a4d6d4cf912c60093a8b
Felix Joeken <felix@joeken.eu>
2025-03-29 15:57:06 +0100

    Update models.py. This brakes the app temporary

--- nlonline_antrag/models.py
+++ nlonline_antrag/models.py
@@ -4,15 +4,9 @@ from django.db import models
 # Create your models here.
 class NLOnlineAntrag(models.Model):
     application_date = models.DateTimeField(auto_now_add=True)
-
-    first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
-
     street = models.CharField(max_length=100)
     zip_code = models.CharField(max_length=100)
     city = models.CharField(max_length=100)
-
-    email = models.EmailField(max_length=100)
 
     phone = models.CharField(max_length=100)
 
@@ -29,3 +23,30 @@ class NLOnlineAntrag(models.Model):
     bank_statements = models.FileField(upload_to='bank_statements/')
     identity_document = models.FileField(upload_to='identity_documents/')
     other_documents = models.FileField(upload_to='other_documents/', blank=True)
+
+
+class Applicant(models.Model):
+    application = models.ForeignKey(NLOnlineAntrag, on_delete=models.CASCADE)
+
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+
+    email = models.EmailField(max_length=100)
+    stud_email = models.EmailField(max_length=100)
+
+
+class EarningsExpenses(models.Model):
+    application = models.ForeignKey(NLOnlineAntrag, on_delete=models.CASCADE, related_name='earnings_expenses')
+
+    earning_month = models.DateField()
+
+    earnings_from_parents = models.DecimalField(max_digits=10, decimal_places=2)
+    earnings_from_grants = models.DecimalField(max_digits=10, decimal_places=2)
+    earnings_from_job = models.DecimalField(max_digits=10, decimal_places=2)
+    earnings_from_credit = models.DecimalField(max_digits=10, decimal_places=2)
+    other_earnings = models.DecimalField(max_digits=10, decimal_places=2, blank=True)
+    other_earnings_description = models.CharField(max_length=100, blank=True)
+
+    # expenses_for_rent = models.DecimalField(max_digits=10, decimal_places=2)
+    # expenses_for_food = models.DecimalField(max_digits=10, decimal_places=2)
+    # expenses_for_health_insurance = models.DecimalField(max_digits=10, decimal_places=2)
